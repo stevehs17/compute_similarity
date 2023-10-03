af14a7711c64545a38e7476ae50b09c65b0f4485
S <stevehs17@gmail.com>
2023-10-02 18:37:40 -0700

    wip

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ import io
 import requests
 
 from testhelper import testhelper
+from get_simp_sim import get_simp_sim
 
-def handler(event, context):
-    return testhelper()
+# def handler(event, context):
+#     return testhelper()
 
