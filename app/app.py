8b283f3772b61ec992be93b756bb2da901ce448e
S <stevehs17@gmail.com>
2023-10-02 18:04:27 -0700

    wip

---
+++ app/app.py
@@ -0,0 +1,13 @@
+import sys
+import os
+import json
+import string
+import time
+import io
+import requests
+
+def handler(event, context):
+    return {
+        'statusCode': 200,
+        'body': ' ***** HELLLLOkjkjkjkOOOOOOO *****'
+    }
