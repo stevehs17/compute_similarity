ec9842bd8ddf656ec1a85c63ab1a01efc9dd038b
S <stevehs17@gmail.com>
2023-10-09 16:58:06 -0700

    Added curl command to execute lambda with authentication.

---
+++ curl_get.sh
@@ -0,0 +1,3 @@
+curl --location 'https://vvkvvxcmqgvt3yuedd6hmoxukq0mzeqs.lambda-url.us-west-1.on.aws/' \
+--header 'X-Amz-Date: 20231009T223750Z' \
+--header 'Authorization: AWS4-HMAC-SHA256 Credential=AKIAWTKF3P4ZCU42XCTY/20231009/us-west-1/lambda/aws4_request, SignedHeaders=host;x-amz-date, Signature=4517607c04aac8a71e366df150ca07443e5050326b5cfe7e731cd72dcae9dda0'
