bfabf39c146f291471af7f016d4ad3a955babfa0
S <stevehs17@gmail.com>
2023-10-09 18:33:34 -0700

    wip

--- app/app.py
+++ app/app.py
@@ -2,15 +2,14 @@ import sys
 import os
 import json
 import string
-from time import time
 import io
 import requests
 
 from get_simp_sim import get_simp_sim
 
 def handler(event, context):
-    start = time()
-    out = get_simp_sim()
-    seconds = time() - start
-    print('Seconds =', seconds)
-    return out
+    print('Event:')
+    print(event)
+    print('Context:')
+    print(context)
+    return get_simp_sim()
--- app/get_simp_sim.py
+++ app/get_simp_sim.py
@@ -1,4 +1,5 @@
 import json
+from time import time
 from similarity import compute_pair_similarity
 
 def get_simp_sim():
@@ -6,8 +7,11 @@ def get_simp_sim():
         print('Starting get_simp_sim() execution...')
         a = 'I walk'
         b = 'I talk'
+        start = time()
         sim = compute_pair_similarity(a, b)
-        body = json.dumps({'similarity': sim})
+        secs = time() - start
+        print('compute_pair_similarity seconds:', secs)
+        body = json.dumps({'similarity': sim, 'seconds': secs})
         out = {
             'statusCode': 200,
             'body': body
