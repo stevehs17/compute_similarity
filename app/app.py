e2a4862cc1a1827b4d0a8d66bf35188df78ff9dc
S <stevehs17@gmail.com>
2023-10-09 18:12:30 -0700

    wip

--- app/app.py
+++ app/app.py
@@ -2,11 +2,15 @@ import sys
 import os
 import json
 import string
-import time
+from time import time
 import io
 import requests
 
 from get_simp_sim import get_simp_sim
 
 def handler(event, context):
-    return get_simp_sim()
+    start = time()
+    out = get_simp_sim()
+    seconds = time() - start
+    print('Seconds =', seconds)
+    return out
